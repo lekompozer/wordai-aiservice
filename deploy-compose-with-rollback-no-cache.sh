61dd17e9e99cebac95cf9894fee0da46716f03de
lekompozer <lekompozer@gmail.com>
2025-10-11 21:21:18 +0700

    increase time check deploy

--- deploy-compose-with-rollback-no-cache.sh
+++ deploy-compose-with-rollback-no-cache.sh
@@ -42,9 +42,9 @@ echo "✅ Using Docker Compose command: $DOCKER_COMPOSE_CMD"
 APP_NAME="wordai-aiservice"
 DOCKER_COMPOSE_FILE="docker-compose.yml"
 SERVICE_NAME="ai-chatbot-rag"
-HEALTH_CHECK_DELAY=30 # Seconds to wait before checking health
+HEALTH_CHECK_DELAY=60 # Seconds to wait before checking health
 MAX_HEALTH_RETRIES=3  # Number of health check attempts
-HEALTH_CHECK_INTERVAL=10 # Seconds between health check retries
+HEALTH_CHECK_INTERVAL=15 # Seconds between health check retries
 DOCKER_HUB_USERNAME="${DOCKER_HUB_USERNAME:-lekompozer}"
 NETWORK_NAME="ai-chatbot-network"
 
